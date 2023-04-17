1b7825bc53163520b05087c1973040b6e549f5d5
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-04-17 17:02:30 -0300

    stop ci infinite loop

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,8 @@ app.post("/welcome", auth, (req, res) => {
   res.status(200).json({ mensagem: "Você fez login!" });
 });
 
+/*
 app.listen(3000, () =>
   console.log('REST API server ready at: http://localhost:3000'),
 )
+*/
