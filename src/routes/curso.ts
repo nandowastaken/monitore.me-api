3901eb42feb7baf01f0f2f356ad09a4762972dfd
Fernando Jorge <fernandojorge.cavalcantegomes@gmail.com>
2023-07-03 11:07:35 -0300

    new route to courses

--- src/routes/curso.ts
+++ src/routes/curso.ts
@@ -14,6 +14,21 @@ router.get('/:id', async (req, res) => {
     })
     res.json(curso);
 })
+
+router.post('/', async (req, res) => {
+    try {
+        const { nome } = req.body;
+        const curso = await prisma.curso.create({
+            data: {
+                nome: nome
+            }
+        })
+
+        res.json({curso})
+    } catch {
+        res.status(500).json({error: "Não foi possível criar o curso"})
+    }
+})
   
   module.exports = router;
   
